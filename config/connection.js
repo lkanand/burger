6150acd3f3effc59a65187470046c41a41dd630d
lokesh.k.anand@gmail.com <lokesh.k.anand@gmail.com>
2018-03-02 01:26:00 -0600

    configured JawsDB for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+	connection = mysql.createConnection({
 	host: "127.0.0.1",
 	port: 8889,
 	user: "root",
 	password: "root",
 	database: "burgers_db"
-});
+	});
+}
 
 connection.connect(function(error){
 	if(error)
